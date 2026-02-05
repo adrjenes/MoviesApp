42bae1837b1ee53c25ab103cb5c7f849edfe2c63
AdrianPC <adrjeness@gmail.com>
2026-02-05 13:22:25 +0100

    Added entities

---
+++ MoviesApp.Domain/Entities/MovieStreamingSite.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesApp.Domain.Entities;
+public class MovieStreamingSite
+{
+    public int Id { get; set; }
+    public int MovieId { get; set; }
+    public int StreamingSiteId { get; set; }
+    public Movie Movie { get; set; }
+    public StreamingSite StreamingSite { get; set; }
+}
---
+++ MoviesApp.Domain/Entities/StreamingSite.cs
@@ -0,0 +1,7 @@
+﻿namespace MoviesApp.Domain.Entities;
+public class StreamingSite
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public List<MovieStreamingSite> MovieStreamingSites { get; set; } = new();
+}
